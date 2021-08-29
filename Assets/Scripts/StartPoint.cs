76693c1eb6dd1961af9c93fde0eb6f9efe4d4f87
Juyeong J <juyeong.j@outlook.com>
2021-08-29 23:52:03 +0900

    Finn

--- Assets/Scripts/StartPoint.cs
+++ Assets/Scripts/StartPoint.cs
@@ -16,7 +16,12 @@ public class StartPoint : MonoBehaviour
         theCamera = FindObjectOfType<CameraManager>();
         if (startPoint == thePlayer.currentMapName)
         {
-            if(thePlayer.beforeMapName == startVar)
+            if(thePlayer.beforeMapName == "Prov_Info")
+            {
+                theCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, theCamera.transform.position.z);
+                thePlayer.transform.position = this.transform.position;
+            }
+            else if(thePlayer.beforeMapName == startVar)
             {
                 theCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, theCamera.transform.position.z);
                 thePlayer.transform.position = this.transform.position;
